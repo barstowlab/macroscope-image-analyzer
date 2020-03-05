2d40b063a5c6cc6f95ec9fdd8cc9ee42f661f5d6
Barstowlab <50498803+barstowlab@users.noreply.github.com>
2020-03-05 15:35:27 -0500

    Add scripts for first time

---
+++ OrganizeAssayPhotographsByBarcode.py
@@ -0,0 +1,23 @@
+# Read Barcodes and Organize Files
+
+from macroscopeUtils4B import RenameAndCopyImageFile
+from specutils9 import GenerateFileList
+
+
+systemBaseDir = "/media/psf/Dropbox for Business/"
+# systemBaseDir = "/Users/buz/Dropbox (BarstowLab)/"
+
+
+sourceBaseDir = systemBaseDir \
++ "/BarstowLab Shared Folder/Data/Macroscope Photos/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
+
+destBaseDir = systemBaseDir \
++ "/BarstowLab Shared Folder/Analysis/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
+
+
+fileList = GenerateFileList(directory=sourceBaseDir, regex=".*\.jpg")
+
+for file in fileList:
+	RenameAndCopyImageFile(destBaseDir, sourceBaseDir + '/' + file, \
+	diagnostics=True, preflight=False, organizeIntoDirectories=True)
+
---
+++ OrganizeStoragePlatesByBarcode.py
@@ -0,0 +1,25 @@
+# Read Barcodes and Organize Files
+
+from macroscopeUtils4B import RenameAndCopyImageFile
+from specutils9 import GenerateFileList
+
+
+systemBaseDir = "/media/psf/Dropbox for Business/"
+# systemBaseDir = "/Users/buz/Dropbox (BarstowLab)/"
+
+
+sourceBaseDir = systemBaseDir \
++ "/BarstowLab Shared Folder/Data/Macroscope Photos/2015-11-13 - Library Replication for AQDS Reduction Assay/Photographs Prior to AQDS Intro (47-78)/"
+
+destBaseDir = systemBaseDir \
++ "/BarstowLab Shared Folder/" \
++ "Analysis/2015-11-13 - Library Replication for AQDS Reduction Assay/" \
++ "Photographs Prior to AQDS Intro (47-78)/"
+
+
+fileList = GenerateFileList(directory=sourceBaseDir, regex=".*\.jpg")
+
+for file in fileList:
+	RenameAndCopyImageFile(destBaseDir, sourceBaseDir + '/' + file, \
+	diagnostics=True, preflight=False, organizeIntoDirectories=False)
+
