fb4c528506d228cce2732280b3655ec40889ea33
llj35 <ljj35@cornell.edu>
2020-03-23 11:27:09 -0400

    Added tkinter file selection

--- Organize/OrganizeAssayPhotographsByBarcode.py
+++ Organize/OrganizeAssayPhotographsByBarcode.py
@@ -5,17 +5,25 @@
 
 from macroscopeUtils4B import RenameAndCopyImageFile
 from specutils9 import GenerateFileList
+from tkinter import filedialog
+import tkinter as tk
+
+root = tk.Tk()
+root.withdraw()
+print("select source directory")
+sourceBaseDir = filedialog.askdirectory()
+print("select destination directory")
+destBaseDir = filedialog.askdirectory()
+
+# systemBaseDir = "/media/psf/Dropbox for Business/"
+# # systemBaseDir = "/Users/buz/Dropbox (BarstowLab)/"
 
 
-systemBaseDir = "/media/psf/Dropbox for Business/"
-# systemBaseDir = "/Users/buz/Dropbox (BarstowLab)/"
+# sourceBaseDir = systemBaseDir \
+# + "/BarstowLab Shared Folder/Data/Macroscope Photos/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
 
-
-sourceBaseDir = systemBaseDir \
-+ "/BarstowLab Shared Folder/Data/Macroscope Photos/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
-
-destBaseDir = systemBaseDir \
-+ "/BarstowLab Shared Folder/Analysis/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
+# destBaseDir = systemBaseDir \
+# + "/BarstowLab Shared Folder/Analysis/2015-11-19 - AQDS Reduction with Shewanella Sudoku Library Part 2/"
 
 
 fileList = GenerateFileList(directory=sourceBaseDir, regex=".*\.jpg")
